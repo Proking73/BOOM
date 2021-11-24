4fee5d2e0127d232c08e610e9ab746e4ac4ca433
77035338 <MY_NAME@example.com>
2021-11-24 18:09:10 -0500

    u sure?

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
         function bye() {
+            window.alert("Are you sure you want to press the big, red, button?");
             document.write("<img width='100%' height='1005' src='https://c.tenor.com/o7kwCN9_VjEAAAAC/explosion-boom.gif'>");
         }
         
