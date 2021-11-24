2487de48e8a011e628aadea55706f7e6e05791d5
77035338 <MY_NAME@example.com>
2021-11-24 18:33:25 -0500

    m querys

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
         function bye() {
-            window.alert("Are you sure you want to press the big, red, button?");
-            document.write("<img width='100%' height='1005' src='https://c.tenor.com/o7kwCN9_VjEAAAAC/explosion-boom.gif'>");
+        //    window.alert("Are you sure you want to press the big, red, button?");
+            document.querySelector("body").style.background = "blue";
+            document.write("<img style='border:10vh solid red;' width='100%' height='100%' src='https://c.tenor.com/o7kwCN9_VjEAAAAC/explosion-boom.gif'>");
         }
         
