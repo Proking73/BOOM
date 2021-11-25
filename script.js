a0896cc6b0dc36ae2f7ad484caef2dcef93db73a
The person <77035338+Proking73@users.noreply.github.com>
2021-11-24 20:03:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
         function bye() {
-        //    window.alert("Are you sure you want to press the big, red, button?");
+            window.alert("Are you sure you want to press the big, red, button?");
             document.querySelector("body").style.background = "blue";
-            document.write("<img style='border:10vh solid red;' width='100%' height='100%' src='https://c.tenor.com/o7kwCN9_VjEAAAAC/explosion-boom.gif'>");
+            document.write("<img style='border:1vh solid darkred;margin:none;padding:none;position:absolute;top:0;left:0;box-sizing:border-box;' width='100%' height='100%' src='https://c.tenor.com/o7kwCN9_VjEAAAAC/explosion-boom.gif'>");
         }
         
