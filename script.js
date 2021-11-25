1fdf1d07246ea26dc76902e1bc8654ca5d157e32
The person <77035338+Proking73@users.noreply.github.com>
2021-11-24 20:26:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,6 @@
             document.querySelector("body").style.background = "blue";
             document.write("<img style='border:1vh solid darkred;margin:none;padding:none;position:absolute;top:0;left:0;box-sizing:border-box;' width='100%' height='100%' src='https://c.tenor.com/o7kwCN9_VjEAAAAC/explosion-boom.gif'>");
         }
-        
+        if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  }
